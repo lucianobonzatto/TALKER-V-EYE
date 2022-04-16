32c1387e7db7068abd72941b8137dc984b94fde0
lucianobonzatto <lucianobonzatto@github.com>
2022-04-16 00:31:36 -0300

    include lidar lib to source

--- cProject/source/LidarLite.cpp
+++ cProject/source/LidarLite.cpp
@@ -1,9 +1,16 @@
 #include "LidarLite.h"
 
 LidarLite::LidarLite() {
-
+//    sensor.i2c_init;
+//    sensor.configure(0);
 }
 
 LidarLite::~LidarLite() {
 
+}
+
+int LidarLite::getDistance(){
+//    sensor.takeRange();
+//    sensor.readDistance();
+    return 0;
 }
